cdc4d2fcc5ae1e2daec11e13543daa8811612318
Cristhoffer Santos <nunes-santos@hotmail.com>
2023-01-02 01:00:43 -0300

    test: create an user instance

---
+++ src/entities/User/profile.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from "vitest"
+import { User } from "./User"
+
+test("Create an profile instance", () => {
+  const id = "any_id"
+  const email = "john.doe@mail.com"
+  const hashedPassword = "hashed_password"
+
+  const user = new User(id, email, hashedPassword)
+
+  expect(user).toBeInstanceOf(User)
+  expect(user.hashedPassword).toBe(hashedPassword)
+})
