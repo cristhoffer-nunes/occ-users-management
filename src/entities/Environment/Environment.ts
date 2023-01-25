df5d36bdfe0bd1898d0cef4a9815bd7cfaf3749e
Cristhoffer Santos <nunes-santos@hotmail.com>
2023-01-25 17:18:56 -0300

    feat: add new values

--- src/entities/Environment/Environment.ts
+++ src/entities/Environment/Environment.ts
@@ -3,6 +3,9 @@ export class Environment {
   public name: string
   public url: string
   public appKey: string
+  public email: string
+  public password: string
+  public totp_code: string
 
   constructor(props: Omit<Environment, "id">) {
     Object.assign(this, props)
