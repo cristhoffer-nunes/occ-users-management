7579c89d8f81459040dcfb15053aef77218fca04
Cristhoffer Santos <nunes-santos@hotmail.com>
2023-08-24 23:06:36 -0300

    refactor: change mfaLogin to login by appKey

--- src/modules/oracle/useCases/listProfileByEmail/ListProfileByEmailUseCase.ts
+++ src/modules/oracle/useCases/listProfileByEmail/ListProfileByEmailUseCase.ts
@@ -24,48 +24,22 @@ export class ListProfileByEmailUseCase {
     const environments = await this.environmentsRepository.findByName(
       environmentName
     )
-    const isPRD = environments.environment === "PRD"
-    const adminActive = environments.active === "A"
+    
+    const token = await this.profilesRepository.login({
+      url: environments.url,
+      appKey: environments.appKey
+    })
 
-    if (isPRD && adminActive) {
-      const totpCode = TOTPGenerator(environments.secretKey)
-      const token = await this.profilesRepository.mfaLogin({
-        url: environments.url,
-        email: environments.email,
-        password: environments.password,
-        totp_code: totpCode,
-      })
+    const profile = await this.profilesRepository.findByEmail({
+      email: email,
+      url: environments.url,
+      token: token,
+    })
 
-      const profile = await this.profilesRepository.findByEmail({
-        email: email,
-        url: environments.url,
-        token: token,
-      })
-
-      if (profile != undefined) {
-        return profile
-      } else {
-        throw new AppError("Profile is not registired in this environment.")
-      }
-    } else if (!isPRD && adminActive) {
-      const token = await this.profilesRepository.mfaLogin({
-        url: environments.url,
-        email: environments.email,
-        password: environments.password,
-        totp_code: environments.totp_code,
-      })
-
-      const profile = await this.profilesRepository.findByEmail({
-        email: email,
-        url: environments.url,
-        token: token,
-      })
-
-      if (profile != undefined) {
-        return profile
-      } else {
-        throw new AppError("Profile is not registired in this environment.")
-      }
+    if (profile != undefined) {
+      return profile
+    } else {
+      throw new AppError("Profile is not registired in this environment.")
     }
   }
 }
