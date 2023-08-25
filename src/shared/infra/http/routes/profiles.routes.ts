5748ec496b32c3a9e51b05766374e6a431dc3241
cristhoffer-santos <cristhoffer.santos@jbq.global>
2023-08-24 21:31:57 -0300

    feat: update many profiles by environment

--- src/shared/infra/http/routes/profiles.routes.ts
+++ src/shared/infra/http/routes/profiles.routes.ts
@@ -1,21 +1,23 @@
+import { Router } from "express"
 import { CreateProfileController } from "@modules/oracle/useCases/createProfile/CreateProfileController"
 import { ListManyProfilesController } from "@modules/oracle/useCases/listManyProfiles/ListManyProfilesController"
 import { ListProfileByEmailController } from "@modules/oracle/useCases/listProfileByEmail/ListProfileByEmailController"
-import { RequestPasswordResetController } from "@modules/oracle/useCases/requestPasswordReset/RequestPasswordResetController"
+import { ListAllProfilesController } from "@modules/oracle/useCases/listAllProfiles/ListAllProfilesController"
 import { DisableProfileByManyEnvironmentsController } from "@modules/oracle/useCases/disableProfileByManyEnvironments/DisableProfileByManyEnvironmentsController"
 import { UpdateProfileController } from "@modules/oracle/useCases/updateProfile/UpdateProfileController"
-import { Router } from "express"
-import { ListAllProfilesController } from "@modules/oracle/useCases/listAllProfiles/ListAllProfilesController"
+import { UpdateManyProfilesController } from "@modules/oracle/useCases/updateManyProfiles/UpdateManyProfilesController"
+import { RequestPasswordResetController } from "@modules/oracle/useCases/requestPasswordReset/RequestPasswordResetController"
 
 const profilesRoutes = Router()
+const createProfileController = new CreateProfileController()
 const listManyProfilesController = new ListManyProfilesController()
 const listProfileByEmailController = new ListProfileByEmailController()
 const listAllProfilesController = new ListAllProfilesController()
 const disableProfileByManyEnvironmentsController =
   new DisableProfileByManyEnvironmentsController()
 const updateProfileController = new UpdateProfileController()
+const updateManyProfilesController = new UpdateManyProfilesController()
 const requestPasswordResetController = new RequestPasswordResetController()
-const createProfileController = new CreateProfileController()
 
 profilesRoutes.post("/list", listManyProfilesController.handle)
 profilesRoutes.post("/listByEmail", listProfileByEmailController.handle)
@@ -25,6 +27,7 @@ profilesRoutes.put(
   disableProfileByManyEnvironmentsController.handle
 )
 profilesRoutes.put("/update", updateProfileController.handle)
+profilesRoutes.put("/updateMany", updateManyProfilesController.handle)
 profilesRoutes.post(
   "/requestPasswordReset",
   requestPasswordResetController.handle
